111f020be7432a8901c4ffc092a7e595a771cc68
Annie-Claude Morin <annie@pleo.io>
2021-12-22 09:37:11 -0500

    Day 1 Part 2 done!

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Advent of Code 2021
 
+## Setup
+
+You just want python3 installed. I'm trying to do this without any additional
+modules so you should be good to go with Python 3 installed!
+
 ## Day 1
 
-To run the code for day 1, just run `python3 part-1.py` in the `day-1` folder. That's it!
+To run the code for day 1, just run `python3 part-1.py` in the `day-1` folder.
+That's it! Similarly for part 2, you can just run `python3 part-2.py`,
---
+++ day-1/part-2.py
@@ -0,0 +1,18 @@
+def windowed(list, windowSize):
+    for i in range(len(list) - windowSize + 1):
+        yield list[i:i+windowSize]
+
+inputFile = open('input.txt', 'r')
+lines = list(map(int, inputFile.readlines()))
+
+count = 0
+windows = list(windowed(lines, 3))
+lastWindowSum = None
+for window in windows:
+    currentWindowSum = sum(window)
+    if lastWindowSum is not None and currentWindowSum > lastWindowSum: 
+        count += 1
+
+    lastWindowSum = currentWindowSum
+
+print(count)
