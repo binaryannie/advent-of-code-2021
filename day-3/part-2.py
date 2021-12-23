ee462fa2c1b44f28162ce258f040a192ada1120d
BinaryAnnie <binaryannie@hotmail.com>
2021-12-23 08:14:39 -0500

    Almost a day 2 solution

---
+++ day-3/part-2.py
@@ -0,0 +1,36 @@
+def findMostCommonValue(reportLines, digit):
+    return str(int(sum(map(lambda line: int(line[digit]), reportLines)) > len(reportLines)/2))
+
+def findLeastCommonValue(reportLines, digit):
+    return str(int(sum(map(lambda line: int(line[digit]), reportLines)) < len(reportLines)/2))
+
+inputFile = open('input.txt', 'r')
+reportLines = list(map(lambda line: line.rstrip('\n'), inputFile.readlines()))
+
+numberOfReportLines = len(reportLines)
+reportLineLength = len(reportLines[0])
+oxygenGeneratorRating = None
+co2ScrubberRating = None
+possibleOgr = reportLines
+possibleCsr = reportLines
+
+for i in range(0, reportLineLength):
+    mostCommonValue = findMostCommonValue(possibleOgr, i)
+    possibleOgr = list(filter(lambda line: line[i] == mostCommonValue, possibleOgr))
+    if(len(possibleOgr) == 1 and oxygenGeneratorRating is None):
+        oxygenGeneratorRating = possibleOgr[0]
+
+    leastCommonValue = findLeastCommonValue(possibleCsr, i)
+    possibleCsr = list(filter(lambda line: line[i] == leastCommonValue, possibleCsr))
+    if(len(possibleCsr) == 1 and co2ScrubberRating is None):
+        co2ScrubberRating = possibleCsr[0]
+
+    if(oxygenGeneratorRating is not None and co2ScrubberRating is not None):
+        break
+    
+
+print(oxygenGeneratorRating, co2ScrubberRating)
+
+oxygenGeneratorRating = int(oxygenGeneratorRating, 2)
+co2ScrubberRating = int(co2ScrubberRating, 2)
+print(oxygenGeneratorRating * co2ScrubberRating)
